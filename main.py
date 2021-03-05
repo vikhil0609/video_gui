50e3ab16e893cc0b05505824f5b38bb404c2ed3f
Mahipal108 <mahipalsundvesha@gmail.com>
2021-03-05 12:43:20 +0530

    add browse feature

--- main.py
+++ main.py
@@ -9,7 +9,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -150,12 +150,24 @@ class Ui_Dialog(object):
         self.pushButton_5.setText(_translate("Dialog", "DEMO"))
         self.pushButton_6.setText(_translate("Dialog", "DEMO"))
         self.pushButton_7.setText(_translate("Dialog", "Browse Input Video"))
+        self.pushButton_7.clicked.connect(self.pushButton_handler)
         self.pushButton_8.setText(_translate("Dialog", "Browse ground frame image"))
         self.pushButton_9.setText(_translate("Dialog", "Play selected video"))
         self.pushButton_10.setText(_translate("Dialog", "Close"))
         self.label.setText(_translate("Dialog", "VIDEO"))
 
+    def pushButton_handler(self):
+        # print("Button pressed")
+        self.open_dialog_box()
+        
+    def open_dialog_box(self):
+        filename = QFileDialog.getOpenFileName()
+        path = filename[0]
+        print(path)
 
+        with open(path, "r") as f:
+            print(f.readline())
+            
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
